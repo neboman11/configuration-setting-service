707f7ba1f12b8a1e0ece62f68d0520dc212c6b45
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2024-11-09 17:06:55 -0600

    Changed database connection string to be built internally

--- src/database.rs
+++ src/database.rs
@@ -13,7 +13,17 @@ pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
 lazy_static! {
     static ref POOL: Pool = {
-        let db_url = env::var("DATABASE_URL").expect("Database url not set");
+        let db_host = env::var("DATABASE_HOST").expect("Database host not set");
+        let db_user = env::var("DATABASE_USER").expect("Database user not set");
+        let db_password = env::var("DATABASE_PASSWORD").expect("Database password not set");
+        let db_database = env::var("DATABASE_DATABASE").expect("Database name not set");
+        let db_port = env::var("DATABASE_PORT")
+            .or::<Result<String, &str>>(Ok("5432".to_string()))
+            .unwrap();
+        let db_url = format!(
+            "postgres://{}:{}@{}:{}/{}",
+            db_user, db_password, db_host, db_port, db_database
+        );
         let manager = ConnectionManager::<PgConnection>::new(db_url);
         Pool::new(manager).expect("Failed to create db pool")
     };
